787504fd9e492e3b1150dbe1f4806d357db7cb2c
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2024-07-29 15:25:23 +0700

    push public

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -3,8 +3,8 @@
   <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="{{ route('dashboard') }}">
-        <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="...">
-        <span class="ms-3 font-weight-bold">Telkom Akses KDT Dashboard</span>
+        <img src="../assets/img/AksesKu.png" class="navbar-brand-img h-100" alt="AksesKu Telkom Akses">
+        <span class="ms-3 font-weight-bold"><b>AksesKu Telkom Kedaton</b></span>
     </a>
   </div>
   <hr class="horizontal dark mt-0">
