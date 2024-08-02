1b9498b288cf76cc4b7cda2198920046bd4c700a
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2024-08-02 15:20:58 +0700

    push repo

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,30 +2,47 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../assets/css/errors.css" type="text/css">
-    <title>404 Not Found</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page 404 Abstract</title>
+    <!--Style-->
+    <link rel="stylesheet" href="../../assets/css/404.css">
 </head>
 <body>
-   <section class="page_404">
-       <div class="container">
-           <div class="row">
-               <div class="col-sm-12">
-                   <div class="col-sm-10 col-sm-offset-1 text-center">
-                       <div class="four_zero_four_bg">
-                           <h1 class="text-center">404</h1>
-                       </div>
-                       <div class="content_box_404">
-                           <h3 class="h2">Looks Like You're Lost</h3>
-                           <p>The page you are looking for not available</p>
-                           <a href="/dashboard">Go to Home</a>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
-   </section>
+
+<!--Square-->
+<div class="Square404" id="Square">
+    <div class="Square">
+        <!--404 inside the square-->
+        <h1>404</h1>
+    </div>
+</div>
+
+<!--Texts-->
+<div class="texts">
+    <h4>Oops! Page not found</h4>
+    <p>The page you are looking for does not exist. Go back to the main page or search.</p>
+    <button onclick="history.back()" class="btn">Go Back</button>
+    <!-- <a href="../../index.html" class="btn" onclick="history.back()" >Back to Home</a> -->
+    <label for="search_box"></label><input type="search" name="search" id="search_box" placeholder="Search">
+</div>
+
+<script>
+    let container1 = document.getElementById('Square');
+    <!--The main square movement script when moving the mouse-->
+    window.onmousemove = function (e) {
+        let x = -e.x / 90,
+            y = -e.y / 90;
+
+        container1.style.right = x + 'px';
+        container1.style.bottom = y + 'px';
+    }
+    /* Mobile gyroscope */
+    window.addEventListener("deviceorientation", function (e) {
+        container1.style.right = e.gamma / 3 + "px";
+        container1.style.bottom = e.beta / 3 + "px";
+    });
+</script>
 </body>
 </html>
