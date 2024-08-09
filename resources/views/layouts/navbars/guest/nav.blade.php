bb24a72c37a0fdb156ab143dc8350bcfbfc98bbb
Kevin Simorangkir <104813623+kevinsimorangkir21@users.noreply.github.com>
2024-08-09 10:28:28 +0700

    push nav

--- resources/views/layouts/navbars/guest/nav.blade.php
+++ resources/views/layouts/navbars/guest/nav.blade.php
@@ -1,3 +1,9 @@
+<!-- Bootstrap CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Bundle with Popper (includes Bootstrap's JavaScript and Popper.js) -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 my-3 {{ (Request::is('static-sign-up') ? 'w-100 shadow-none  navbar-transparent mt-4' : 'blur blur-rounded shadow py-2 start-0 end-0 mx4') }}">
   <div class="container-fluid {{ (Request::is('static-sign-up') ? 'container' : 'container-fluid') }}">
@@ -41,7 +47,7 @@
           <a class="nav-link" href="https://www.creative-tim.com/presentation" target="_blank">Team</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="https://www.creative-tim.com/templates" target="_blank">Products</a>
+          <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#developerModal">Developer</a>
         </li>
       </ul>
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -56,3 +62,44 @@
   </div>
 </nav>
 <!-- End Navbar -->
+
+<!-- Modal -->
+<div class="modal fade" id="developerModal" tabindex="-1" aria-labelledby="developerModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="developerModalLabel">Developer Information</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body text-center">
+        <div class="row">
+          <!-- Developer 1 -->
+          <div class="col-md-6 mb-3">
+            <img src="path-to-developer1-photo.jpg" alt="Developer 1 Photo" class="img-fluid rounded-circle mb-2" style="max-width: 150px;">
+            <h4>Developer 1 Name</h4>
+            <p>Brief description or role of Developer 1.</p>
+          </div>
+          <!-- Developer 2 -->
+          <div class="col-md-6 mb-3">
+            <img src="path-to-developer2-photo.jpg" alt="Developer 2 Photo" class="img-fluid rounded-circle mb-2" style="max-width: 150px;">
+            <h4>Developer 2 Name</h4>
+            <p>Brief description or role of Developer 2.</p>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Modal -->
+
+
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+  var myModal = new bootstrap.Modal(document.getElementById('developerModal'), {});
+});
+
+</script>
+
